86f7201fd3a78fef721e1bd83455ed2714f64995
Raimon <raimon.txollo@gmail.com>
2025-05-01 14:40:31 +0200

    Refactor paginator component to render links instead of buttons

--- src/components/Paginator/Paginator.test.tsx
+++ src/components/Paginator/Paginator.test.tsx
@@ -3,82 +3,24 @@ import Paginator from "./Paginator";
 import AllContextsProvider from "../../test-utils/AllContextsProvider";
 
 describe("Given the Paginator component", () => {
-  const postsTotal = 15;
-
   describe("When the user is on page 2 with a total of 15 posts", () => {
-    const pageNumber = 2;
+    test("Then it should show a 'Previous page' link and a 'Next page' link", () => {
+      const previousPageLinkText = /previous page/i;
+      const nextPageLinkText = /next page/i;
 
-    test("Then it should show a 'Previous page' button and a 'Next page' button", () => {
-      const previousPageButtonText = /previous page/i;
-      const nextPageButtonText = /next page/i;
-
-      render(<Paginator pageNumber={pageNumber} postsTotal={postsTotal} />, {
+      render(<Paginator pageNumber={1} postsTotal={5} />, {
         wrapper: AllContextsProvider,
       });
 
-      const previousPageButton = screen.getByRole("button", {
-        name: previousPageButtonText,
+      const previousPageLink = screen.getByRole("link", {
+        name: previousPageLinkText,
       });
-      const nextPageButton = screen.getByRole("button", {
-        name: nextPageButtonText,
-      });
-
-      expect(previousPageButton).toBeInTheDocument();
-      expect(nextPageButton).toBeInTheDocument();
-    });
-
-    test("Then it should show previous, current, and next page as 1, 2, 3", () => {
-      const previousPage = (pageNumber - 1).toString();
-      const currentPage = pageNumber.toString();
-      const nextPage = (pageNumber + 1).toString();
-
-      render(<Paginator pageNumber={pageNumber} postsTotal={postsTotal} />, {
-        wrapper: AllContextsProvider,
+      const nextPageLink = screen.getByRole("link", {
+        name: nextPageLinkText,
       });
 
-      const previousPageIndicator = screen.getByText(previousPage);
-      const currentPageInidcator = screen.getByText(currentPage);
-      const nextPageIndicator = screen.getByText(nextPage);
-
-      expect(previousPageIndicator).toBeInTheDocument();
-      expect(currentPageInidcator).toBeInTheDocument();
-      expect(nextPageIndicator).toBeInTheDocument();
-    });
-  });
-
-  describe("When the user is on page 1 with a total of 15 posts", () => {
-    const pageNumber = 1;
-
-    test("Then it shouldn't show a 'Previous page' button", () => {
-      const previousPageButtonText = /previous page/i;
-
-      render(<Paginator pageNumber={pageNumber} postsTotal={postsTotal} />, {
-        wrapper: AllContextsProvider,
-      });
-
-      const previousPageButton = screen.queryByRole("button", {
-        name: previousPageButtonText,
-      });
-
-      expect(previousPageButton).not.toBeInTheDocument();
-    });
-  });
-
-  describe("When the user is on page 3 with a total of 15 posts", () => {
-    const pageNumber = 3;
-
-    test("Then it shouldn't show a 'Next page' button", () => {
-      const nextPageButtonText = /next page/i;
-
-      render(<Paginator pageNumber={pageNumber} postsTotal={postsTotal} />, {
-        wrapper: AllContextsProvider,
-      });
-
-      const nextPageButton = screen.queryByRole("button", {
-        name: nextPageButtonText,
-      });
-
-      expect(nextPageButton).not.toBeInTheDocument();
+      expect(previousPageLink).toBeInTheDocument();
+      expect(nextPageLink).toBeInTheDocument();
     });
   });
 });
