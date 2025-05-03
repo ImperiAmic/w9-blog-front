d6efa5357d4feb391acc1847cf24e8cc903312e3
Raimon <raimon.txollo@gmail.com>
2025-05-03 17:59:36 +0200

    Add PostDetail component and styles

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import App from "../components/App/App";
 import NotFoundPage from "../pages/NotFoundPage/NotFoundPage";
 import PostsPage from "../post/pages/PostsPage/PostsPage";
 import PostFormPage from "../post/pages/PostFormPage/PostFormPage";
+import PostDetail from "../post/pages/PostDetail/PostDetail";
 
 const AppRouter: React.FC = () => {
   return (
@@ -12,6 +13,7 @@ const AppRouter: React.FC = () => {
         <Route path="posts" element={<PostsPage />} />
         <Route path="posts" element={<PostsPage />} />
         <Route path="add-post" element={<PostFormPage />} />
+        <Route path="post/:postId" element={<PostDetail />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
